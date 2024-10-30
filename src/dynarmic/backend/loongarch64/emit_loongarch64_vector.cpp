f6c3c0f85960cead1bc2c63b8ed5c0005b85c645
Jia Qingtong <jiaqingtong@huawei.com>
2024-10-30 23:10:14 +0800

    Emit: check vsrai's imm

--- src/dynarmic/backend/loongarch64/emit_loongarch64_vector.cpp
+++ src/dynarmic/backend/loongarch64/emit_loongarch64_vector.cpp
@@ -412,7 +412,7 @@ namespace Dynarmic::Backend::LoongArch64 {
     void EmitIR<IR::Opcode::VectorArithmeticShiftRight8>(BlockOfCode &code, EmitContext &ctx,
                                                          IR::Inst *inst) {
         EmitImmShift(code, ctx, inst, [&](auto Vresult, auto Voperand, u8 shift_amount) {
-            code.vsrai_b(Vresult, Voperand, shift_amount  & 0x7);
+            code.vsrai_b(Vresult, Voperand, shift_amount > 0x7 ? 0x7 : shift_amount);
         });
     }
 
@@ -420,7 +420,7 @@ namespace Dynarmic::Backend::LoongArch64 {
     void EmitIR<IR::Opcode::VectorArithmeticShiftRight16>(BlockOfCode &code, EmitContext &ctx,
                                                           IR::Inst *inst) {
         EmitImmShift(code, ctx, inst, [&](auto Vresult, auto Voperand, u8 shift_amount) {
-            code.vsrai_h(Vresult, Voperand, shift_amount & 0xf);
+            code.vsrai_h(Vresult, Voperand, shift_amount > 0xf ? 0xf : shift_amount);
         });
     }
 
@@ -428,7 +428,7 @@ namespace Dynarmic::Backend::LoongArch64 {
     void EmitIR<IR::Opcode::VectorArithmeticShiftRight32>(BlockOfCode &code, EmitContext &ctx,
                                                           IR::Inst *inst) {
         EmitImmShift(code, ctx, inst, [&](auto Vresult, auto Voperand, u8 shift_amount) {
-            code.vsrai_w(Vresult, Voperand, shift_amount & 0x1f);
+            code.vsrai_w(Vresult, Voperand, shift_amount > 0x1f ? 0x1f : shift_amount);
         });
     }
 
@@ -436,7 +436,7 @@ namespace Dynarmic::Backend::LoongArch64 {
     void EmitIR<IR::Opcode::VectorArithmeticShiftRight64>(BlockOfCode &code, EmitContext &ctx,
                                                           IR::Inst *inst) {
         EmitImmShift(code, ctx, inst, [&](auto Vresult, auto Voperand, u8 shift_amount) {
-            code.vsrai_d(Vresult, Voperand, shift_amount & 0x3f);
+            code.vsrai_d(Vresult, Voperand, shift_amount > 0x3f ? 0x3f : shift_amount);
         });
     }
 
